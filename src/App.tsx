d4cf984ac789a078fcf67c33a83010e4b814b46d
Gabriel Salustriano <gabrielsalustriano5566@gmail.com>
2022-07-23 13:35:51 -0300

    Implementação: data e types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+
+
 
 const App = () => {
+  const [list, setList] = useState(items);
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+        {/* Área de Informações */}
+
+        {/* Área de Inserção */}
+
+        {/* Tabela de Itens */}
+
       </C.Body>
     </C.Container>
   );
