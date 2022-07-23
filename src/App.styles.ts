e7bbed23d5127434653f1a7e1772ba6256f79da3
Gabriel Salustriano <gabrielsalustriano5566@gmail.com>
2022-07-23 13:35:51 -0300

    Implementação: pasta 'data' e 'types'

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -15,4 +15,8 @@ export const HeaderText = styled.h1`
     padding-top: 30px;
 `;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 5, 23), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 5, 23), category: 'food', title: 'BurgerKing', value: 28 },
+    { date: new Date(2022, 5, 24), category: 'rent', title: 'Aluguel', value: 800 },
+    { date: new Date(2022, 6, 4), category: 'salary', title: 'Salário', value: 4500 },
+];
