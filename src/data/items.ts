28de85cd95759a1b09167aabac85391ff0e519d5
Gabriel Salustriano <gabrielsalustriano5566@gmail.com>
2022-07-23 17:19:00 -0300

    Alpha 0.1

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,5 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2022, 6, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2022, 6, 15), category: 'food', title: 'Burger King', value: 28 },
-    { date: new Date(2022, 6, 16), category: 'rent', title: 'Aluguel Apt', value: 800 },
-    { date: new Date(2022, 5, 18), category: 'salary', title: 'Salário Acme', value: 4500 },
+
 ];
